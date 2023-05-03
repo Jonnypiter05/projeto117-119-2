d9a8154d21e660fb8f0a2e4be67de9e41ed8a544
Jonnypiter05 <111024941+Jonnypiter05@users.noreply.github.com>
2023-05-03 18:56:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+//criar a function setup//
+function setup() 
+{
+    canvas = createCanvas(280,280);
+    canvas.center();
+    background("white");
+}
+
+function clearCanvas(){
+    background("white");
+}
